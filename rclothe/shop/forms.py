020c67641c1214791f6ccfb589087a60b2140763
Janmay Bhatt <jrbhatt18p10@gmail.com>
2022-05-30 15:46:13 +0530

    second commit

---
+++ rclothe/shop/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+
+class NewsLetterSubscription(forms.Form):
+    email = forms.EmailField(max_length=128)
+    source = forms.CharField(       # A hidden input for internal use
+        max_length=50,              # tell from which page the user sent the message
+        widget=forms.HiddenInput()
+    )
+
+    def clean(self):
+        cleaned_data = super(NewsLetterSubscription, self).clean()
+        email = cleaned_data.get('email')
+        if not email:
+            raise forms.ValidationError('You have to write something!')
